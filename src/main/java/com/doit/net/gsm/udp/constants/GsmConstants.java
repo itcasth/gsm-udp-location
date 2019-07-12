19e5eca042a9350e1ef7e60a135dcc5bb5b82523
wanglinyong <1290300229@qq.com>
2019-07-12 13:34:25 +0800

    创建GSM消息

--- src/main/java/com/doit/net/gsm/udp/constants/GsmConstants.java
+++ src/main/java/com/doit/net/gsm/udp/constants/GsmConstants.java
@@ -12,6 +12,28 @@ public class GsmConstants {
 	public static final String ONE_UEINFO_INDI = "103";//103 OneUeInfoIndi
 	public static final String STATUS_RPT_INDI = "116";//116 StatusRptIndi
 
+	public static final String SEND_SET_PARAM = "SetRfPara ";
 	public static final String SEND_GET_PARAM = "GetRfPara";
 	public static final String SEND_HEATBEAT = "HeatBeat";
+	public static final String START_CELL = "StartCell";
+	public static final String STOP_CELL = "StopCell";
+	/**
+	 * 名单字符串 15 个字符对应一个 IMSI 最大支持 100 个 IMSI
+	 */
+	public static final String SET_BLACK_LIST = "SetBlackList ";
+	/**
+	 0 ：上号模式
+	 1：定位黑名单模式 首选
+	 2: 定位黑名单模式 2 对于不连续上报手机 备选 仅用于 4G 模式
+	 3：管控黑名单模式
+	 4：管控所有模式
+	 5：定位指定手机模式 仅用于 4G 模式
+	 */
+	public static final String WORK_TYPE = "SetWorkType ";
+
+
+	public static final String REBOOT = "Reboot 0";
+	public static final String SetTacUpdate = "SetTacUpdate ";//SetTacUpdate +分钟数
+	public static final String StartReportMeasure = "StartReportMeasure ";
+	public static final String StopReportMeasure = "StopReportMeasure ";
 }
---
+++ src/main/java/com/doit/net/gsm/udp/message/GsmMessageCreator.java
@@ -0,0 +1,145 @@
+package com.doit.net.gsm.udp.message;
+
+import com.doit.net.gsm.udp.base.GsmMessage;
+import com.doit.net.gsm.udp.constants.GsmConstants;
+import com.doit.net.gsm.udp.server.GsmSenderThread;
+
+/**
+ * Created by wly on 2019/7/12.
+ * GSM消息处理器
+ */
+public class GsmMessageCreator {
+	/**
+	 * 开启射频
+	 * @param ip
+	 * @param port
+	 */
+	public static void openRf(String ip,int port){
+		GsmMessage message = new GsmMessage();
+		message.setSocketAddress(ip,port);
+		message.setContent( GsmConstants.START_CELL );
+		GsmSenderThread.put(message);
+	}
+
+	/**
+	 * 关闭射频
+	 * @param ip
+	 * @param port
+	 */
+	public static void closeRf(String ip,int port){
+		GsmMessage message = new GsmMessage();
+		message.setSocketAddress(ip,port);
+		message.setContent( GsmConstants.START_CELL );
+		GsmSenderThread.put(message);
+	}
+
+
+	/**
+	 * 心跳
+	 * @param ip
+	 * @param port
+	 */
+	public static void heartBeat(String ip,int port){
+		GsmMessage message = new GsmMessage();
+		message.setSocketAddress(ip,port);
+		message.setContent( GsmConstants.SEND_HEATBEAT );
+		GsmSenderThread.put(message);
+	}
+
+	/**
+	 * 工作模式
+	 * @param ip
+	 * @param port
+	 */
+	public static void setWorkType(String ip,int port,int mode){
+		GsmMessage message = new GsmMessage();
+		message.setSocketAddress(ip,port);
+		message.setContent( GsmConstants.WORK_TYPE+mode );
+		GsmSenderThread.put(message);
+	}
+
+	/**
+	 * 设置黑名单
+	 * @param ip
+	 * @param port
+	 */
+	public static void setBlackList(String ip,int port,String imsi){
+		GsmMessage message = new GsmMessage();
+		message.setSocketAddress(ip,port);
+		message.setContent( GsmConstants.SET_BLACK_LIST+imsi );
+		GsmSenderThread.put(message);
+	}
+
+
+	/**
+	 * 获取参数
+	 * @param ip
+	 * @param port
+	 */
+	public static void getRfPara(String ip,int port){
+		GsmMessage message = new GsmMessage();
+		message.setSocketAddress(ip,port);
+		message.setContent( GsmConstants.SEND_GET_PARAM );
+		GsmSenderThread.put(message);
+	}
+
+	/**
+	 * 设置参数
+	 * @param ip
+	 * @param port
+	 */
+	public static void setRfPara(String ip,int port,String mcc,String mnc,String arfcn,String bsic,String lac,String ci,String power ){
+		GsmMessage message = new GsmMessage();
+		message.setSocketAddress(ip,port);
+		message.setContent( GsmConstants.SEND_SET_PARAM+mcc+" "+mnc+" "+ arfcn +" "+ bsic+" "+lac+" "+ci+" "+power+" 30 0" );
+		GsmSenderThread.put(message);
+	}
+
+	/**
+	 * 开始测量上报（用于4G）
+	 * @param ip
+	 * @param port
+	 */
+	public static void startReportMeasure(String ip,int port,String imsi){
+		GsmMessage message = new GsmMessage();
+		message.setSocketAddress(ip,port);
+		message.setContent( GsmConstants.StartReportMeasure+imsi );
+		GsmSenderThread.put(message);
+	}
+
+	/**
+	 * 停止测量上报（用于4G）
+	 * @param ip
+	 * @param port
+	 */
+	public static void stopReportMeasure(String ip,int port,String imsi){
+		GsmMessage message = new GsmMessage();
+		message.setSocketAddress(ip,port);
+		message.setContent( GsmConstants.StopReportMeasure+imsi );
+		GsmSenderThread.put(message);
+	}
+
+	/**
+	 * 重启设备
+	 * @param ip
+	 * @param port
+	 */
+	public static void stopReportMeasure(String ip,int port){
+		GsmMessage message = new GsmMessage();
+		message.setSocketAddress(ip,port);
+		message.setContent( GsmConstants.REBOOT );
+		GsmSenderThread.put(message);
+	}
+
+	/**
+	 * 设置TAC周期
+	 * @param ip
+	 * @param port
+	 */
+	public static void setTacUpdate(String ip,int port,String minute){
+		GsmMessage message = new GsmMessage();
+		message.setSocketAddress(ip,port);
+		message.setContent( GsmConstants.SetTacUpdate+minute );
+		GsmSenderThread.put(message);
+	}
+}
